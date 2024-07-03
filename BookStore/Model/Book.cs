100e916abe4d93a7d0a6f3982b82aaaf45291f82
mutlu kokdemir <mutlu.kokdemir@outlook.com>
2024-07-03 23:17:04 +0300

    chore: Initializes the service

---
+++ BookStore/Model/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace BookStore.Model
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public int PublishingYear { get; set; }
+
+        public int Price { get; set; }
+
+        public double Rate { get; set; }
+
+        public List<Comment> Comments { get; set; }
+    }
+}
---
+++ BookStore/Model/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStore.Model
+{
+    public class Comment
+    {
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public string Message { get; set; }
+    }
+}
